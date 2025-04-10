9fb57c01bd8a179381fd27749082480bea3d8260
Diogo  Alberto <kirchoffalberto@gmail.com>
2025-04-09 22:30:09 -0300

    🧱fix: Problem with tailwind not functional

--- app-giphy/quasar.config.js
+++ app-giphy/quasar.config.js
@@ -17,7 +17,7 @@ export default defineConfig((/* ctx */) => {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#css
     css: [
-      'app.css'
+      'tailwind.css'
     ],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
