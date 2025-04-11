88ec840c650be914730f37bf113987358e4063b9
Diogo  Alberto <kirchoffalberto@gmail.com>
2025-04-11 01:42:08 -0300

    🧱fix: remove route not utilized

--- app-giphy/src/router/routes.js
+++ app-giphy/src/router/routes.js
@@ -9,13 +9,6 @@ const routes = [
       { path: 'about', component: () => import('pages/AboutPage.vue') },
     ]
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
 ]
 
 export default routes
